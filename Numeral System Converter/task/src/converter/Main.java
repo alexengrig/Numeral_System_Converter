0d60c09a7bfe18bb32d702476817533b76337596
Grig Alex <alexengrigdev@gmail.com>
2019-12-15 21:05:06 +0300

    Add 4/6 solution

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -5,14 +5,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        int radix = scanner.nextInt();
-        if (radix == 2) {
-            System.out.println("0b" + Integer.toBinaryString(number));
-        } else if (radix == 8) {
-            System.out.println("0" + Integer.toOctalString(number));
-        } else if (radix == 16) {
-            System.out.println("0x" + Integer.toHexString(number));
+        int sourceRadix = scanner.nextInt();
+        String sourceNumber = scanner.next();
+        int targetRadix = scanner.nextInt();
+        int number;
+        if (sourceRadix == 1) {
+            number = sourceNumber.length();
+        } else {
+            number = Integer.parseInt(sourceNumber, sourceRadix);
         }
+        String targetNumber = "";
+        if (targetRadix == 1) {
+            for (int i = 0; i < number; i++) {
+                targetNumber += "1";
+            }
+        } else {
+            targetNumber = Integer.toString(number, targetRadix);
+        }
+        System.out.println(targetNumber);
     }
 }
