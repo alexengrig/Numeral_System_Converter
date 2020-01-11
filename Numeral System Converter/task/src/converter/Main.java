5755698c0cd281425bc379a915b555e6fa583820
Grig Alex <alexengrigdev@gmail.com>
2020-01-11 22:13:52 +0300

    Add 6/6 solution

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -1,15 +1,44 @@
 package converter;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int sourceRadix = scanner.nextInt();
-        String sourceNumber = scanner.next();
-        int targetRadix = scanner.nextInt();
+        List<String> lines = new java.util.ArrayList<>(List.of());
+        while (scanner.hasNextLine() && lines.size() != 3) {
+            lines.add(scanner.nextLine());
+        }
+        if (lines.size() != 3) {
+            error();
+            return;
+        }
+        String sourceRadix = lines.get(0);
+        String radixRegex = "([1-9]|1[0-9]|2[0-9]|3[0-6])";
+        if (!sourceRadix.matches(radixRegex)) {
+            error();
+            return;
+        }
+        String sourceNumber = lines.get(1);
+        if (!sourceNumber.matches("([1-9a-z][0-9a-z]*(\\.[0-9a-z]*)?|0\\.[0-9a-z]+)")) {
+            error();
+            return;
+        }
+        String targetRadix = lines.get(2);
+        if (!targetRadix.matches(radixRegex)) {
+            error();
+            return;
+        }
         Converter converter = new Converter();
-        String targetNumber = converter.convert(sourceNumber, sourceRadix, targetRadix);
+        String targetNumber = converter.convert(
+                sourceNumber,
+                Integer.parseInt(sourceRadix),
+                Integer.parseInt(targetRadix));
         System.out.println(targetNumber);
+    }
+
+    private static void error() {
+        System.out.println("error");
     }
 }
--- Numeral System Converter/task/test/ConverterTest.java
+++ Numeral System Converter/task/test/ConverterTest.java
@@ -1,17 +1,14 @@
-import converter.Converter;
 import converter.Main;
 import org.hyperskill.hstest.v6.stage.BaseStageTest;
 import org.hyperskill.hstest.v6.testcase.CheckResult;
 import org.hyperskill.hstest.v6.testcase.TestCase;
-import org.junit.Assert;
-import org.junit.Test;
 
 import java.util.List;
 
 class Clue {
     final String input;
-    final boolean provideAnswer;
     String answer;
+    final boolean provideAnswer;
 
     Clue(final String input, final String answer, final boolean provideAnswer) {
         this.input = input;
@@ -37,70 +34,108 @@ public class ConverterTest extends BaseStageTest<Clue> {
 
     static TestCase<Clue> testToAnswer(final String input, final String answer, final boolean provideAnswer) {
         return new TestCase<Clue>()
-                .setAttach(new Clue(input, answer, provideAnswer))
-                .setInput(input);
+            .setAttach(new Clue(input, answer, provideAnswer))
+            .setInput(input);
     }
 
     @Override
     public List<TestCase<Clue>> generate() {
         return List.of(
-                /* Tests with a hint: */
-                testToAnswer("10\n0.234\n7", "0.14315", true),
-                testToAnswer("10\n10.234\n7", "13.14315", true),
-                testToAnswer("6\n2.555\n1", "11", true),
-                testToAnswer("35\naf.xy\n17", "148.g88a8", true),
-                testToAnswer("10\n11\n2", "1011", true),
-                testToAnswer("16\naaaaa.0\n24", "22df2.00000", true),
-                testToAnswer("16\n0.cdefb\n24", "0.j78da", true),
-                testToAnswer("16\naaaaa.cdefb\n24", "22df2.j78da", true),
+            /* Tests with a hint: */
+            testToAnswer("a b\n\n\n", null, true),
+            testToAnswer("11\nabc\n\n", null, true),
+            testToAnswer("11\n1\ndas\n", null, true),
+            testToAnswer("0\n1\ndas\n", null, true),
+            testToAnswer("37\n1\ndas\n", null, true),
+            testToAnswer("36\n1\n0\n", null, true),
+            testToAnswer("36\n1\n37\n", null, true),
 
-                /* Tests without a hint: */
-                testToAnswer("10\n0.2340\n7", "0.14315", false),
-                testToAnswer("10\n10.2340\n7", "13.14315", false),
-                testToAnswer("6\n2.5550\n1", "11", false),
-                testToAnswer("35\naf.xy0\n17", "148.g88a8", false),
-                testToAnswer("10\n12\n2", "1100", false),
-                testToAnswer("16\naaaaa.00\n24", "22df2.00000", false),
-                testToAnswer("16\n0.cdefb0\n24", "0.j78da", false),
-                testToAnswer("16\naaaaa.cdefb0\n24", "22df2.j78da", false),
+            /* Tests without a hint: */
+            testToAnswer("c\n\n\n", null, false),
+            testToAnswer("12\nbc\n\n", null, false),
+            testToAnswer("12\n1\nhf\n", null, false),
+            testToAnswer("0\n2\nhf\n", null, false),
+            testToAnswer("37\n2\nhf\n", null, false),
+            testToAnswer("14\n2\n-1\n", null, false),
+            testToAnswer("14\n2\n38\n", null, false),
 
-                /* Tests from previous stage (with a hint): */
-                testToAnswer("10\n11\n2\n", "1011", true),
-                testToAnswer("1\n11111\n10\n", "5", true),
-                testToAnswer("10\n1000\n36\n", "rs", true),
-                testToAnswer("21\n4242\n6\n", "451552", true),
-                testToAnswer("7\n12\n11\n", "9", true),
-                testToAnswer("5\n300\n10\n", "75", true),
-                testToAnswer("1\n11111\n5\n", "10", true),
-                testToAnswer("10\n4\n1\n", "1111", true),
+            /* Tests from previous stage (with a hint): */
+            testToAnswer("10\n0.234\n7", "0.14315", true),
+            testToAnswer("10\n10.234\n7", "13.14315", true),
+            testToAnswer("6\n2.555\n1", "11", true),
+            testToAnswer("35\naf.xy\n17", "148.g88a8", true),
+            testToAnswer("10\n11\n2", "1011", true),
+            testToAnswer("16\naaaaa.0\n24", "22df2.00000", true),
+            testToAnswer("16\n0.cdefb\n24", "0.j78da", true),
+            testToAnswer("16\naaaaa.cdefb\n24", "22df2.j78da", true),
 
-                /* Tests from previous stage (without a hint): */
-                testToAnswer("10\n12\n2\n", "1100", false),
-                testToAnswer("1\n1111111\n10\n", "7", false),
-                testToAnswer("10\n1001\n36\n", "rt", false),
-                testToAnswer("21\n4243\n6\n", "451553", false),
-                testToAnswer("7\n13\n11\n", "a", false),
-                testToAnswer("5\n301\n10\n", "76", false),
-                testToAnswer("1\n111111\n5\n", "11", false),
-                testToAnswer("10\n5\n1\n", "11111", false)
+            /* Tests from previous stage (without a hint): */
+            testToAnswer("10\n0.2340\n7", "0.14315", false),
+            testToAnswer("10\n10.2340\n7", "13.14315", false),
+            testToAnswer("6\n2.5550\n1", "11", false),
+            testToAnswer("35\naf.xy0\n17", "148.g88a8", false),
+            testToAnswer("10\n12\n2", "1100", false),
+            testToAnswer("16\naaaaa.00\n24", "22df2.00000", false),
+            testToAnswer("16\n0.cdefb0\n24", "0.j78da", false),
+            testToAnswer("16\naaaaa.cdefb0\n24", "22df2.j78da", false),
+
+            /* Tests from previous stage (with a hint): */
+            testToAnswer("10\n11\n2\n", "1011", true),
+            testToAnswer("1\n11111\n10\n", "5", true),
+            testToAnswer("10\n1000\n36\n", "rs", true),
+            testToAnswer("21\n4242\n6\n", "451552", true),
+            testToAnswer("7\n12\n11\n", "9", true),
+            testToAnswer("5\n300\n10\n", "75", true),
+            testToAnswer("1\n11111\n5\n", "10", true),
+            testToAnswer("10\n4\n1\n", "1111", true),
+
+            /* Tests from previous stage (without a hint): */
+            testToAnswer("10\n12\n2\n", "1100", false),
+            testToAnswer("1\n1111111\n10\n", "7", false),
+            testToAnswer("10\n1001\n36\n", "rt", false),
+            testToAnswer("21\n4243\n6\n", "451553", false),
+            testToAnswer("7\n13\n11\n", "a", false),
+            testToAnswer("5\n301\n10\n", "76", false),
+            testToAnswer("1\n111111\n5\n", "11", false),
+            testToAnswer("10\n5\n1\n", "11111", false)
         );
     }
 
     @Override
     public CheckResult check(String reply, Clue clue) {
         final String[] lines = reply
-                .lines()
-                .filter(line -> !line.isEmpty())
-                .toArray(String[]::new);
+            .lines()
+            .filter(line -> !line.isEmpty())
+            .toArray(String[]::new);
 
         if (lines.length == 0) {
             return new CheckResult(
-                    false,
-                    "Your program doesn't print any line."
+                false,
+                "Your program doesn't print any line."
             );
         }
 
         String answer = lines[lines.length - 1];
+
+        if (clue.answer == null) {
+            if (answer.toLowerCase().contains("error")) {
+                return new CheckResult(true);
+            } else if (clue.provideAnswer) {
+                return new CheckResult(
+                    false,
+                    "Your program doesn't say about an error.\n" +
+                        "This is a sample test so we give you a hint.\n" +
+                        "Input: " + clue.input + "\n" +
+                        "Your answer: " + answer
+                );
+            } else {
+                return new CheckResult(
+                    false,
+                    "Your program doesn't say about an error."
+                );
+            }
+        }
+
         answer = answer.replaceAll("[^\\p{Graph}]", "");
         clue.answer = clue.answer.replaceAll("[^\\p{Graph}]", "");
 
@@ -110,17 +145,17 @@ public class ConverterTest extends BaseStageTest<Clue> {
         if (!answer.equals(clue.answer)) {
             if (clue.provideAnswer) {
                 return new CheckResult(
-                        false,
-                        "Your answer is wrong.\n" +
-                                "This is a sample test so we give you a hint.\n" +
-                                "Input: " + clue.input + "\n" +
-                                "Your answer: " + answer + "\n" +
-                                "Correct answer: " + clue.answer
+                    false,
+                    "Your program gives a wrong answer when there is no error in the input.\n" +
+                        "This is a sample test so we give you a hint.\n" +
+                        "Input: " + clue.input + "\n" +
+                        "Your answer: " + answer + "\n" +
+                        "Correct answer: " + clue.answer
                 );
             } else {
                 return new CheckResult(
-                        false,
-                        "Your answer is wrong."
+                    false,
+                    "Your program gives a wrong answer when there is no error in the input."
                 );
             }
         }
@@ -139,69 +174,5 @@ public class ConverterTest extends BaseStageTest<Clue> {
             number = number.substring(0, number.length() - 1);
         }
         return number;
-    }
-
-    // My tests
-
-    @Test
-    public void check_binary_to_decimal() {
-        Converter converter = new Converter();
-        int result = converter.convertToDecimal("11101000", 2);
-        Assert.assertEquals(232, result);
-    }
-
-    @Test
-    public void check_octal_to_decimal() {
-        Converter converter = new Converter();
-        int result = converter.convertToDecimal("75013", 8);
-        Assert.assertEquals(31243, result);
-    }
-
-    @Test
-    public void check_hex_to_decimal() {
-        Converter converter = new Converter();
-        int result = converter.convertToDecimal("fda1", 16);
-        Assert.assertEquals(64929, result);
-    }
-
-    @Test
-    public void check_decimal_to_binary() {
-        Converter converter = new Converter();
-        String result = converter.convertFromDecimal(22, 2);
-        Assert.assertEquals("10110", result);
-    }
-
-    @Test
-    public void check_decimal_to_octal() {
-        Converter converter = new Converter();
-        String result = converter.convertFromDecimal(571, 8);
-        Assert.assertEquals("1073", result);
-    }
-
-    @Test
-    public void check_decimal_to_hex() {
-        Converter converter = new Converter();
-        String result = converter.convertFromDecimal(7467, 16);
-        Assert.assertEquals("1d2b", result);
-    }
-
-    @Test
-    public void check_10(){
-        Converter converter = new Converter();
-        String r = converter.convert("1000", 10, 36);
-        Assert.assertEquals("rs", r);
-    }
-    @Test
-    public void check_fraction(){
-        Converter converter = new Converter();
-        String result = converter.convertFromFraction(0.234, 7);
-        Assert.assertEquals("143", result.substring(0, 3));
-    }
-
-    @Test
-    public void check_convert() {
-        Converter converter = new Converter();
-        String result = converter.convert("1010", 2, 16);
-        Assert.assertEquals("a", result);
     }
 }
